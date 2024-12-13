2b7b196cb0cabe808234f6d67b287e6be363d5e0
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-12 20:33:07 -0500

    used react Liks instead of href

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import Login from '../components/Login'
+import { Link } from 'react-router-dom'
+
 function navbar() {
     
     const [theme, setTheme] = useState(localStorage.getItem('theme')?localStorage.getItem('theme'):'light');
@@ -38,9 +40,9 @@ function navbar() {
     )
     const navItems = (
     <>
-        <li><a href = '/'>Home</a></li>
-        <li><a href = '/course'>Course</a></li>
-        <li><a href = '/contact'>Contact</a></li>
+        <li><Link to ='/'>Home</Link></li>
+        <li><Link to ='/course'>Course</Link></li>
+        <li><Link to ='/contact'>Contact</Link></li>
         {/* <li><a href = '/about'>About</a></li> */}
     </>
     )
